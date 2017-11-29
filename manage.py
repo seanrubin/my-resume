1ed443d861ff0c32f075853a13cd3bc2b5c2a021
Sean Rubin <seanrubi@udel.edu>
2017-11-29 15:54:59 -0500

    Created the Professor and Course databases

--- manage.py
+++ manage.py
@@ -1,12 +1,26 @@
 from flask_script import Manager
-from my_resume import app
+from my_resume import app, db, Professor, Course
 
 manager = Manager(app)
 
 
 @manager.command
-def hello():
-    print "hello"
+def deploy():
+    db.drop_all()
+    db.create_all()
+    prof1 = Professor(name='Serva,Mark A', department='MISY')
+    prof2 = Professor(name='Vanleer,Michael G', department='ACCT')
+    course1 = Course(course_no='MISY330', title="Database Design and Implementation", description="Covers the design and implementation of enterprise databases", professor=prof1)
+    course2 = Course(course_no='MISY350', title="Business Application Development II", description="Covers concepts related to client side development", professor=prof1)
+    course3 = Course(course_no='ACCT208', title="Accounting II", description="Introduction to managerial accounting.", professor=prof2)
+    course4 = Course(course_no='ACCT207', title="Accounting I", description="An introduction to financial accounting.", professor=prof2)
+    db.session.add(prof1)
+    db.session.add(prof2)
+    db.session.add(course1)
+    db.session.add(course2)
+    db.session.add(course3)
+    db.session.add(course4)
+    db.session.commit()
 
 
 if __name__ == "__main__":
--- my_resume.py
+++ my_resume.py
@@ -1,22 +1,120 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, session, render_template, request, flash, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess secure key'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
+
+# setup SQLAlchemy
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+db = SQLAlchemy(app)
+
+
+# define database tables
+class Professor(db.Model):
+    __tablename__ = 'professors'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    department = db.Column(db.Text)
+    courses = db.relationship('Course', backref='professor')
+
+
+class Course(db.Model):
+    __tablename__ = 'courses'
+    id = db.Column(db.Integer, primary_key=True)
+    course_no = db.Column(db.String(256))
+    title = db.Column(db.Integer)
+    description = db.Column(db.Text)
+    professor_id = db.Column(db.Integer, db.ForeignKey('professors.id'))
 
 
 @app.route('/')
 def index():
-    # return HTML
-    # return "<h1>this is the index page!<h1>"
     return render_template('index.html')
 
 
+@app.route('/professors')
+def show_all_professors():
+    professors = Professor.query.all()
+    return render_template('professor-all.html', professors=professors)
+
+
+@app.route('/professor/add', methods=['GET', 'POST'])
+def add_professors():
+    if request.method == 'GET':
+        return render_template('professor-add.html')
+    if request.method == 'POST':
+        # get data from the form
+        name = request.form['name']
+        department = request.form['department']
+
+        # insert the data into the database
+        professor = Professor(name=name, department=department)
+        db.session.add(professor)
+        db.session.commit()
+        return redirect(url_for('show_all_professors'))
+
+
+@app.route('/professor/edit/<int:id>', methods=['GET', 'POST'])
+def edit_artist(id):
+    professor = Professor.query.filter_by(id=id).first()
+    if request.method == 'GET':
+        return render_template('professor-edit.html', professor=professor)
+    if request.method == 'POST':
+        # update data based on the form data
+        professor.name = request.form['name']
+        professor.department = request.form['department']
+        # update the database
+        db.session.commit()
+        return redirect(url_for('show_all_professors'))
+
+
+    # course-all.html adds song id to the edit button using a hidden input
 @app.route('/courses')
 def show_all_courses():
-    courses = [
-        'MISY330',
-        'MISY261',
-        'MISY225'
-    ]
-    return render_template('all-courses.html', courses=courses)
+    courses = Course.query.all()
+    return render_template('course-all.html', courses=courses)
+
+
+@app.route('/course/add', methods=['GET', 'POST'])
+def add_courses():
+    if request.method == 'GET':
+        professors = Professor.query.all()
+        return render_template('course-add.html', professors=professors)
+    if request.method == 'POST':
+        # get data from the form
+        course_no = request.form['course_no']
+        title = request.form['title']
+        description = request.form['description']
+        professor_name = request.form['professor']
+        professor = Professor.query.filter_by(name=professor_name).first()
+        course = Course(course_no=course_no, title=title, description=description, professor=professor)
+
+        # insert the data into the database
+        db.session.add(course)
+        db.session.commit()
+        return redirect(url_for('show_all_courses'))
+
+
+@app.route('/course/edit/<int:id>', methods=['GET', 'POST'])
+def edit_course(id):
+    course = Course.query.filter_by(id=id).first()
+    professors = Professor.query.all()
+    if request.method == 'GET':
+        return render_template('course-edit.html', course=course, professors=professors)
+    if request.method == 'POST':
+        # update data based on the form data
+        course.course_no = request.form['course_no']
+        course.title = request.form['title']
+        course.description = request.form['description']
+        professor_name = request.form['professor']
+        professor = Professor.query.filter_by(name=professor_name).first()
+        course.professor = professor
+        # update the database
+        db.session.commit()
+        return redirect(url_for('show_all_courses'))
 
 
 @app.route('/about')
@@ -24,35 +122,7 @@ def about():
     return render_template('about.html')
 
 
-@app.route('/users')
-def show_all_users():
-    # return "<h2>this is the page for all users</h2>"
-    return render_template('user-all.html')
 
 
-@app.route('/user/<string:name>/')
-def get_user_name(name):
-    # return "hello " + name
-    # return "Hello %s, this is %s" % (name, 'administrator')
-    return render_template('user.html', name=name)
-
-
-
-
-@app.route('/course/<int:id>/')
-def get_course_id(id):
-    # return "This course's ID is " + str(id)
-    return "Hi, this is %s and the course's id is %d" % ('administrator', id)
-
-
-# https://goo.gl/Pc39w8 explains the following line
 if __name__ == '__main__':
-
-    # activates the debugger and the reloader during development
-    # app.run(debug=True)
-    app.run()
-
-    # make the server publicly available on port 80
-    # note that Ports below 1024 can be opened only by root
-    # you need to use sudo for the following conmmand
-    # app.run(host='0.0.0.0', port=80)
+        app.run()
