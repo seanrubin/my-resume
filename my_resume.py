f9399c076ffe546c3c79cbd9401589b3bf27296e
Sean Rubin <seanrubi@udel.edu>
2017-12-03 17:40:33 -0500

    Added delete functions

--- my_resume.py
+++ my_resume.py
@@ -18,7 +18,7 @@ class Professor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64))
     department = db.Column(db.Text)
-    courses = db.relationship('Course', backref='professor')
+    courses = db.relationship('Course', backref='professor', cascade="delete")
 
 
 class Course(db.Model):
@@ -58,7 +58,7 @@ def add_professors():
 
 
 @app.route('/professor/edit/<int:id>', methods=['GET', 'POST'])
-def edit_artist(id):
+def edit_professor(id):
     professor = Professor.query.filter_by(id=id).first()
     if request.method == 'GET':
         return render_template('professor-edit.html', professor=professor)
@@ -71,7 +71,20 @@ def edit_artist(id):
         return redirect(url_for('show_all_professors'))
 
 
-    # course-all.html adds song id to the edit button using a hidden input
+@app.route('/professor/delete/<int:id>', methods=['GET', 'POST'])
+def delete_professor(id):
+    professor = Professor.query.filter_by(id=id).first()
+    if request.method == 'GET':
+        return render_template('professor-delete.html', professor=professor)
+    if request.method == 'POST':
+        # delete the professor by id
+        # all related courses are deleted as well
+        db.session.delete(professor)
+        db.session.commit()
+        return redirect(url_for('show_all_professors'))
+
+
+    # course-all.html adds course id to the edit button using a hidden input
 @app.route('/courses')
 def show_all_courses():
     courses = Course.query.all()
@@ -113,6 +126,20 @@ def edit_course(id):
         professor = Professor.query.filter_by(name=professor_name).first()
         course.professor = professor
         # update the database
+        db.session.commit()
+        return redirect(url_for('show_all_courses'))
+
+
+@app.route('/course/delete/<int:id>', methods=['GET', 'POST'])
+def delete_course(id):
+    course = Course.query.filter_by(id=id).first()
+    professors = Professor.query.all()
+    if request.method == 'GET':
+        return render_template('course-delete.html', course=course, professors=professors)
+    if request.method == 'POST':
+        # use the id to delete the course
+        # course.query.filter_by(id=id).delete()
+        db.session.delete(course)
         db.session.commit()
         return redirect(url_for('show_all_courses'))
 
